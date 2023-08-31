f8ebff5f8e997b36e65b0d750b6d63ccc8d0fa0c
Marcelo Oliveira <marce@DESKTOP-BR12D8K>
2023-08-31 17:45:37 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using PrimeiroProjeto;
+using System;
+using System.Globalization;
+namespace PrimeiroProjetoOriatacaoObjt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do trinagulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            Console.WriteLine("Entre com as medidas do trinagulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Area de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
